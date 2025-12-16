fda56ec44506818302b43ca4dd143a03d92f3ab3
Antho_Off <113532396+Twitch-Antho@users.noreply.github.com>
2025-12-16 09:15:28 +0100

    questions.py

---
+++ Code Source/Père Noel/Team Royale/questions.py
@@ -0,0 +1,8 @@
+QUESTIONS = {
+    1: "Je suis grand quand je suis jeune et petit quand je suis vieux. Qui suis-je ?",
+    2: "Quel mois de l’année a 28 jours ?",
+    3: "Qu’est-ce qui appartient à toi mais que les autres utilisent plus que toi ?",
+    4: "Je peux voler sans ailes. Qui suis-je ?",
+    # ...
+    # jusqu'à 24
+}
