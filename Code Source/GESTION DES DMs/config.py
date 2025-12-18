cf37a0c64cbfdd861fc4d7542fb1afac368cbf7e
Antho_Off <113532396+Twitch-Antho@users.noreply.github.com>
2025-12-18 10:33:01 +0100

    config.py

---
+++ Code Source/GESTION DES DMs/config.py
@@ -0,0 +1,5 @@
+# config.py
+TOKEN = "TON_TOKEN_DISCORD_ICI"
+PREFIX = "/"
+FERNET_KEY = b"TA_CLE_FERNET_ICI"  # Générer via Fernet.generate_key() et garder secrète
+LOG_FILE = "logs/dmshield_logs.enc"
